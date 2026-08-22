02e0f41714b8cbd31e7e275cd4afa3a0e8e778ab
rio <1778342750@qq.com>
2026-08-22 19:26:15 +0800

    Delete main.py

--- main.py
+++
@@ -1,8 +0,0 @@
-﻿import gymnasium as gym 
-from environment import GridWorldEnv
-from qlearningagent import QLearningAgent
-
-# env = GridWorldEnv()
-env = gym.make("FrozenLake-v1", is_slippery=False)
-model = QLearningAgent(env)
-model.learn(total_timesteps=30000)  # 训练步数，可按需调整；原来 3000000 步会跑很久
